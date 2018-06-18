1e38bc916415aeaaccfd6e813841456916928b13
YY <z3388638@gmail.com>
2018-06-18 20:23:43 +0800

    add jumbo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+	Container
+} from 'reactstrap';
+
+import Jumbo from './Jumbo';
+
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-			</div>
+			<Container fluid>
+				<Jumbo />
+			</Container>
 		);
 	}
 }
-
-export default App;
---
+++ src/Jumbo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+	Row,
+	Col
+} from 'reactstrap';
+
+import './Jumbo.css';
+import ProfileImg from './profile.jpg';
+
+export default class Jumbo extends React.Component {
+	render() {
+		return (
+			<Row className="Jumbo">
+				<Col className="Jumbo__container" md={ 12 }>
+					<div>
+						<img className="Jumbo__profileImg" src={ ProfileImg } alt="profile" />
+					</div>
+					<div className="Jumbo__name">
+						<span>Y.Y. Chang</span>
+					</div>
+					<div>
+						<a className="Jumbo__socialLink" href="https://github.com/x3388638" rel="noopener noreferrer" target="_blank">
+							<i className="fa fa-github-square"></i> x3388638
+						</a>
+						<a className="Jumbo__socialLink" href="https://www.linkedin.com/in/yuyingchang/" rel="noopener noreferrer" target="_blank">
+							<i className="fa fa-linkedin-square"></i> yuyingchang
+						</a>
+						<a className="Jumbo__socialLink" href="mailto:z3388638@gmail.com" rel="noopener noreferrer" target="_blank">
+							<i className="fa fa-envelope-open"></i> z3388638@gmail.com
+						</a>
+					</div>
+				</Col>
+			</Row>
+		);
+	}
+}
