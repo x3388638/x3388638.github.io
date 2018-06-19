46a946b93d0b061925d652875382b6883ae4e942
YY <z3388638@gmail.com>
2018-06-19 18:23:17 +0800

    add link to project

--- src/BlockTitle.js
+++ src/BlockTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class BlockTitle extends React.Component {
 	render() {
 		return (
-			<div className="BlockTitle">
+			<div id={ this.props.title } className="BlockTitle">
 				<h4><i className={`fa fa-${ this.props.icon }`} aria-hidden="true"></i> { this.props.title }</h4>
 				<hr />
 			</div>
