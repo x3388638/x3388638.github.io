6d7ef7f267eb6e9dfd6154f6c5fa22c1083a0146
YY <z3388638@gmail.com>
2018-07-06 20:22:34 +0800

    migrate styled-components to <Skill />

--- src/Skill.js
+++ src/Skill.js
@@ -5,20 +5,74 @@ import {
 	Row,
 	Card
 } from 'reactstrap';
+import styled from 'styled-components';
 import BlockTitle from './BlockTitle';
 import Loading from './Loading';
 
-import './Skill.css';
+const CardLabelWrapper = styled.div`
+	margin-bottom: 20px;
+`;
+
+const CardLabelContent = styled.span`
+	position: absolute;
+    left: -10px;
+    top: 10px;
+    display: inline-block;
+    background: ${ (props) => `var(--${ props.color })` };
+    width: 50px;
+	text-align: center;
+	color: var(--light);
+	&::after {
+		content: '';
+		display: inline-block;
+		position: absolute;
+		right: -15px;
+		width: 0;
+		height: 0;
+		border-style: solid;
+		border-width: 12px 0 12px 15px;
+		border-color: ${ (props) => `transparent transparent transparent var(--${ props.color })` };
+	}
+`;
 
 class CardLabel extends React.Component {
 	render() {
 		return (
-			<div className="CardLabel">
-				<span className={`CardLabel__content ${ this.props.color }`}>{ this.props.text }</span>
-			</div>
+			<CardLabelWrapper>
+				<CardLabelContent color={ this.props.color }>{ this.props.text }</CardLabelContent>
+			</CardLabelWrapper>
 		)
 	}
 }
+
+const SkillWrapepr = styled(Container)`
+	padding-top: 20px;
+	color: var(--dark);
+`;
+
+const SkillCOntent = styled.div`
+	margin-left: 40px;
+	@media (max-width: 576px) {
+		margin-left: 0;
+	}
+`;
+
+const  SkillBlock = styled(Col)`
+	margin-bottom: 10px;
+`;;
+
+const SkillItem = styled(Col)`
+	text-align: center;
+`;
+
+const SkillText = styled.a`
+	cursor: pointer;
+	display: block;
+	margin-bottom: 5px;
+	&:hover {
+		background: #f3f3f3;
+	}
+`;
 
 export default class Skill extends React.Component {
 	constructor(props) {
@@ -38,7 +92,7 @@ export default class Skill extends React.Component {
 
 	render() {
 		return (
-			<Container className="Skill">
+			<SkillWrapepr>
 				<Row>
 					<Col md={ 12 }>
 						<BlockTitle
@@ -49,13 +103,13 @@ export default class Skill extends React.Component {
 				</Row>
 				<Row>
 					<Col>
-						<div className="Skill__content">
+						<SkillCOntent>
 							{ !this.state.skills &&
 								<Loading />
 							}
 							{ !!this.state.skills &&
 								<Row>
-									<Col className="Skill__block" md={6}>
+									<SkillBlock md={6}>
 										<Card body>
 											<CardLabel color="success" text="熟悉" />
 											{ this.state.skills &&
@@ -68,27 +122,27 @@ export default class Skill extends React.Component {
 												</Row>
 											}
 										</Card>
-									</Col>
-									<Col className="Skill__block" md={6}>
+									</SkillBlock>
+									<SkillBlock md={6}>
 										<Card body>
 											<CardLabel color="yellow" text="略懂" />
 											{ this.state.skills &&
 												<Row>
 													{ this.state.skills[1].map((skill, i) => (
-														<Col key={i} className="Skill__item" xs={6}>
-															<a className="Skill__text" href={skill.link} target="_blank" rel="noopener noreferrer">{skill.text}</a>
-														</Col>
+														<SkillItem key={i} xs={6}>
+															<SkillText href={skill.link} target="_blank" rel="noopener noreferrer">{skill.text}</SkillText>
+														</SkillItem>
 													))}
 												</Row>
 											}
 										</Card>
-									</Col>
+									</SkillBlock>
 								</Row>
 							}
-						</div>
+						</SkillCOntent>
 					</Col>
 				</Row>
-			</Container>
+			</SkillWrapepr>
 		)
 	}
 }
