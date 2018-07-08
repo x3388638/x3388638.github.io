fdd98b0265dd01f63c3d85840521c46e27146899
YY <z3388638@gmail.com>
2018-07-08 22:32:30 +0800

    styled-components for <Footer />

--- src/Footer.js
+++ src/Footer.js
@@ -3,14 +3,25 @@ import {
 	Row,
 	Col
 } from 'reactstrap';
+import styled from 'styled-components';
 
-import './Footer.css';
 import SETTINGS from './settings';
+
+const FooterWrapper = styled(Row)`
+	background: var(--dark);
+	color: var(--light);
+	text-align: center;
+	font-size: 14px;
+    font-weight: 300;
+    padding-top: 50px;
+	padding-bottom: 20px;
+	margin-top: 20px;
+`;
 
 export default class Footer extends React.Component {
 	render() {
 		return (
-			<Row className="Footer">
+			<FooterWrapper>
 				<Col>
 					{ (new Date()).getFullYear() } &copy; { SETTINGS.name }
 					<div>
@@ -18,7 +29,7 @@ export default class Footer extends React.Component {
 						<a className="github-button" href="https://github.com/x3388638/x3388638.github.io/fork" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork x3388638/x3388638.github.io on GitHub">Fork</a>
 					</div>
 				</Col>
-			</Row>
+			</FooterWrapper>
 		)
 	}
 }
