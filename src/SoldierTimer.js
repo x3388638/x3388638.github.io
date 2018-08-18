26ccd5d34a6dd4cf9594ccf9380c1286cefbcbc2
YY <z3388638@gmail.com>
2018-08-18 15:45:37 +0800

    update style

--- src/SoldierTimer.js
+++ src/SoldierTimer.js
@@ -2,7 +2,16 @@ import React from 'react';
 import moment from 'moment';
 import styled from 'styled-components';
 
-const Timer = styled.div``;
+const Timer = styled.span`
+	color: #999;
+	font-size: 12px;
+	margin-bottom: 10px;
+	border: 1px solid #999;
+	border-radius: 5px;
+	padding: 1px 8px;
+	display: inline-block;
+	background: #eaeaea;
+`;
 
 export default (props) => {
 	const revive = moment(props.end).add(1, 'days').diff(moment(), 'day') <= 0;
@@ -13,6 +22,6 @@ export default (props) => {
 	const isDead = moment(props.start).add(1, 'days').diff(moment(), 'day') <= 0;
 	const days = isDead ? moment(props.end).add(1, 'days').diff(moment(), 'day') : moment(props.start).add(1, 'days').diff(moment(), 'day');
 	return (
-		<Timer>{ `倒數 ${ days } 天${ isDead ? '退伍' : '入伍' }` }</Timer>
+		<Timer>{ `倒數 ${ days } 天${ isDead ? '退伍' : '入伍' }QQ` }</Timer>
 	);
 }
